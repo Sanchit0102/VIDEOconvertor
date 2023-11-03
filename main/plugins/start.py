e50e891ce13af42be03b453819f5b1f6bf62fda5
Sanchit0102 <137860157+Sanchit0102@users.noreply.github.com>
2023-11-03 20:17:48 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -46,8 +46,8 @@ async def notice(event):
 async def source(event):
     await event.edit(source_text,
                     buttons=[[
-                         Button.url("FOR PERSONAL USE", url="https://github.com/vasusen-code/videoconvertor/tree/main"),
-                         Button.url("FOR YOUR CHANNEL ", url="https://github.com/vasusen-code/videoconvertor/")]])
+                         Button.url("SUPPORT", url="https://t.me/ROBO_5_0"),
+                         Button.url("DEVELOPER ", url="https://t.me/THE_DS_OFFICIAL")]])
                     
 @Drone.on(events.callbackquery.CallbackQuery(data="help"))
 async def help(event):
